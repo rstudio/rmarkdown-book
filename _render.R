9ea8e09405fdd9a11ab2ce1065d9add466eb7d4b
Christophe Dervieux <christophe.dervieux@gmail.com>
2022-02-18 16:06:30 +0100

    close #85: add workflow for auto build and deployment of book (#100)

--- _render.R
+++ _render.R
@@ -6,10 +6,29 @@ if (length(formats) == 0) formats = c('bookdown::pdf_book', 'bookdown::gitbook')
 # render the book to all formats unless they are specified via command-line args
 for (fmt in formats) {
   cmd = sprintf("bookdown::render_book('index.Rmd', '%s', quiet = %s)", fmt, quiet)
-  res = bookdown:::Rscript(c('-e', shQuote(cmd)))
+  res =  xfun::Rscript(c('-e', shQuote(cmd)))
   if (res != 0) stop('Failed to compile the book to ', fmt)
 }
 
-if (length(formats) > 1 && Sys.getenv('USER') == 'yihui') {
-  bookdown::publish_book(account = 'yihui', server = 'bookdown.org')
+# When several format are rendered, usually when make all is called,
+# then we publish everything to bookdown.org
+if (length(formats) > 1) {
+  message(">> Publishing Books")
+  if (!is.na(Sys.getenv("CI", NA))) {
+    xfun::pkg_load2("rsconnect")
+    # On CI connect to server, using API KEY and deploy using appId
+    rsconnect::addConnectServer('https://bookdown.org', 'bookdown.org')
+    rsconnect::connectApiUser(
+      account = 'GHA', server = 'bookdown.org',
+      apiKey = Sys.getenv('CONNECT_API_KEY')
+    )
+    rsconnect::deploySite(
+      appId = Sys.getenv('CONTENT_ID'),
+      server = 'bookdown.org',
+      render = 'none', logLevel = 'verbose',
+      forceUpdate = TRUE)
+  } else if (Sys.getenv('USER') == 'yihui') {
+    # for local deployment when rsconnect/ is available
+    bookdown::publish_book('rmarkdown', server = 'bookdown.org', render = 'none')
+  }
 }
